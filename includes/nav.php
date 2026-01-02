b199b823c3d38ba6e039bd1e2ea6feae480d13a9
khrastinski <55793803+khrastinski@users.noreply.github.com>
2026-01-02 18:19:48 +0100

    Add files via upload

---
+++ includes/nav.php
@@ -0,0 +1,22 @@
+<nav>
+  <ul>
+    <li><a href="index.php?menu=1">Početna stranica</a></li>
+    <li><a href="index.php?menu=2">Novosti</a></li>
+    <li><a href="index.php?menu=3">Kontakt</a></li>
+    <li><a href="index.php?menu=4">O nama</a></li>
+    <li><a href="index.php?menu=5">Galerija</a></li>
+
+    <?php if (!is_logged_in()): ?>
+      <li><a href="index.php?menu=6">Registracija</a></li>
+      <li><a href="index.php?menu=7">Prijava</a></li>
+    <?php else: ?>
+
+      <?php if (can_access_cms()): ?>
+        <li><a href="index.php?menu=11">Administracija</a></li>
+      <?php endif; ?>
+
+      <li><a href="index.php?menu=9">Odjava</a></li>
+
+    <?php endif; ?>
+  </ul>
+</nav>
